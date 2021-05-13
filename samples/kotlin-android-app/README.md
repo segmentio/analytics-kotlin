6ebfdc2e477200d3599b05d798caf9e7df620401
Prayansh Srivastava <prayansh@gmail.com>
2021-05-13 16:59:34 -0700

    initial push notification tracking stuff

--- samples/kotlin-android-app/README.md
+++ samples/kotlin-android-app/README.md
@@ -22,3 +22,27 @@ Here is how you can do it via adb
 ```bash
 adb shell am start -W -a android.intent.action.VIEW -d "https://segment-sample.com?utm_source=cli\&utm_click=2" com.segment.analytics.next
 ```
+
+## FCM
+This project is setup to track push notification received and opened events. This code is strictly optional and must be customized as per your needs. The code here is only for demonstration purposes
+- Add your project's google-services.json to this folder
+- Modify `MyFirebaseService.kt` to customize the notification displayed to the user.
+- Here is how to send a push notification using cURL [this uses the legacy api](https://firebase.google.com/docs/cloud-messaging/send-message#send-messages-using-the-legacy-app-server-protocols)
+```bash
+curl --request POST \
+  --url https://fcm.googleapis.com/fcm/send \
+  --header 'Authorization: key=<SERVER_KEY>' \
+  --header 'Content-Type: application/json' \
+  --data '{
+	"data": {
+		"title": "Hello World"
+		"content": "You have mail",
+	},
+	"to": "<FCM_TOKEN_FOR_DEVICE>"
+}'
+```
+
+How it works
+- We have 2 core changes
+  - MyFirebaseService.kt
+  - PushNotificationTracking.kt
---
+++ samples/kotlin-android-app/src/main/java/com/segment/analytics/next/plugins/PushNotificationTracking.kt
@@ -0,0 +1,46 @@
+package com.segment.analytics.next.plugins
+
+import android.app.Activity
+import android.os.Bundle
+import android.util.Log
+import com.segment.analytics.Analytics
+import com.segment.analytics.platform.Plugin
+import com.segment.analytics.platform.plugins.android.AndroidLifecycle
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
+
+object PushNotificationTracking: Plugin, AndroidLifecycle {
+    override val type: Plugin.Type = Plugin.Type.Utility
+    override val name: String = "PushNotificationTracking"
+    override lateinit var analytics: Analytics
+
+    override fun onActivityCreated(activity: Activity?, savedInstanceState: Bundle?) {
+        val bundle = if (savedInstanceState == null) {
+            activity?.intent?.extras
+        } else {
+            Bundle().apply {
+                putAll(savedInstanceState)
+                putAll(activity?.intent?.extras)
+            }
+        }
+        checkPushNotification(bundle)
+    }
+
+    private fun checkPushNotification(bundle: Bundle?) {
+        if (bundle != null) {
+            if (bundle.containsKey("push_notification")) {
+                Log.d("PRAY-Plugin", "push notification = yes")
+                analytics.track("Push Notification Tapped", buildJsonObject {
+                    put("action", "Open")
+                    val campaign = buildJsonObject {
+                        put("medium", "Push")
+                        put("source", "FCM")
+                        put("name", bundle.getString("title"))
+                        put("content", bundle.getString("content"))
+                    }
+                    put("campaign", campaign)
+                })
+            }
+        }
+    }
+}
