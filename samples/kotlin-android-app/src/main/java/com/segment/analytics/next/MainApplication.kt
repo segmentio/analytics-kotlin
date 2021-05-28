a4c2943d7cdb08d826bb33df221fec0cba0e2fab
Prayansh Srivastava <prayansh@gmail.com>
2021-05-28 12:15:11 -0700

    remove custom logger from main app

--- samples/kotlin-android-app/src/main/java/com/segment/analytics/next/MainApplication.kt
+++ samples/kotlin-android-app/src/main/java/com/segment/analytics/next/MainApplication.kt
@@ -54,16 +54,5 @@ class MainApplication : Application() {
         analytics.add(WebhookPlugin("https://webhook.site/387c1740-f919-4446-a26e-a9a01ed28c8a", Executors.newSingleThreadExecutor()))
 
         analytics.add(AndroidAdvertisingIdPlugin(this))
-
-        analytics.add(object: Logger("CustomLogger") {
-            override fun log(type: LogType, message: String, event: BaseEvent?) {
-                if (message.contains("Error uploading events from batch file")) {
-                    val dir = applicationContext.getDir("segment-disk-queue", Context.MODE_PRIVATE)
-                    val files = dir.listFiles().map{ it.absolutePath}
-                    Log.d(name, files.toString())
-                    Log.d(name, message)
-                }
-            }
-        })
     }
 }
