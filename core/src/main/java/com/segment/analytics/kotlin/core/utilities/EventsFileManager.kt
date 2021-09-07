bd05448c0e586fe91fb0babc044deb9d03ec6291
Wenxi Zeng <wenxi.zeng@segment.com>
2021-09-07 09:37:01 -0500

    minimize the number of times of open/close file (#32)

--- core/src/main/java/com/segment/analytics/kotlin/core/utilities/EventsFileManager.kt
+++ core/src/main/java/com/segment/analytics/kotlin/core/utilities/EventsFileManager.kt
@@ -1,5 +1,6 @@
 package com.segment.analytics.kotlin.core.utilities
 
+import kotlinx.coroutines.runBlocking
 import java.io.File
 import java.io.FileOutputStream
 import java.time.Instant
@@ -39,9 +40,12 @@ class EventsFileManager(
 
     init {
         createDirectory(directory)
+        registerShutdownHook()
     }
 
     private val fileIndexKey = "segment.events.file.index.$writeKey"
+
+    private var os: FileOutputStream? = null
 
     companion object {
         const val MAX_FILE_SIZE = 475_000 // 475KB
@@ -120,6 +124,8 @@ class EventsFileManager(
         val contents = """],"sentAt":"${Instant.now()}"}"""
         writeToFile(contents.toByteArray(), file)
         file.renameTo(File(directory, file.nameWithoutExtension))
+        os?.close()
+        os = null
         incrementFileIndex()
     }
 
@@ -132,10 +138,22 @@ class EventsFileManager(
     // Atomic write to underlying file
     // TODO make atomic
     private fun writeToFile(content: ByteArray, file: File) {
-        val os = FileOutputStream(file, true)
-        os.write(content)
-        os.flush()
-        os.close()
+        os = os ?: FileOutputStream(file, true)
+        os?.run {
+            write(content)
+            flush()
+        }
+    }
+
+    private fun registerShutdownHook() {
+        // close the stream if the app shuts down
+        Runtime.getRuntime().addShutdownHook(object : Thread() {
+            override fun run() {
+                runBlocking {
+                    os?.close()
+                }
+            }
+        })
     }
 }
 
