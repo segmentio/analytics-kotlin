276f56a3ff8b1d39d1ebe3f5e726ec1ab989eba5
Wenxi Zeng <wenxi.zeng@segment.com>
2021-09-15 07:58:19 -0700

    increase test coverage for HTTPClient.kt

--- core/src/test/kotlin/com/segment/analytics/kotlin/core/HTTPClientTests.kt
+++ core/src/test/kotlin/com/segment/analytics/kotlin/core/HTTPClientTests.kt
@@ -2,9 +2,15 @@ package com.segment.analytics.kotlin.core
 
 import com.segment.analytics.kotlin.core.Constants.LIBRARY_VERSION
 import io.mockk.clearConstructorMockk
+import io.mockk.every
+import io.mockk.spyk
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import java.io.IOException
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class HTTPClientTests {
@@ -48,6 +54,30 @@ class HTTPClientTests {
             assertEquals("gzip", it.getRequestProperty("Content-Encoding"))
             // ideally we would also test if auth Header is set, but due to security concerns this
             // is not possible https://bit.ly/3CVpR3J
+        }
+    }
+
+    @Test
+    fun `safeGetInputStream properly catches exception`() {
+        val connection = spyk(URL("https://api.segment.io/v1/batch").openConnection() as HttpURLConnection)
+        every { connection.inputStream } throws IOException()
+        val errorStream: InputStream? = safeGetInputStream(connection)
+        assertEquals(connection.errorStream, errorStream)
+    }
+
+    @Test
+    fun `createPostConnection close`() {
+        val connection = spyk(httpClient.upload("api.segment.io/v1"))
+        every { connection.connection.responseCode } returns 300
+        every { connection.connection.inputStream } throws IOException()
+        every { connection.connection.responseMessage } returns "test"
+        every { connection.outputStream?.close() } returns Unit
+        try {
+            connection.close()
+        }
+        catch (e: IOException) {
+            e.message?: fail()
+            e.message?.let { assertTrue(it.contains("300")) }
         }
     }
 }
