686bd4acaa0c028305a50b4355c0125f1c8af24f
Prayansh Srivastava <prayansh@gmail.com>
2021-09-24 11:45:31 -0700

    refactor mixpanel destination

--- samples/kotlin-android-app-destinations/src/main/java/com/segment/analytics/destinations/plugins/MixpanelDestination.kt
+++ samples/kotlin-android-app-destinations/src/main/java/com/segment/analytics/destinations/plugins/MixpanelDestination.kt
@@ -4,142 +4,181 @@ import android.app.Activity
 import android.content.Context
 import android.os.Bundle
 import com.mixpanel.android.mpmetrics.MixpanelAPI
-import com.segment.analytics.kotlin.core.*
-import com.segment.analytics.kotlin.core.platform.DestinationPlugin
-import com.segment.analytics.kotlin.core.platform.plugins.LogType
 import com.segment.analytics.kotlin.android.plugins.AndroidLifecycle
 import com.segment.analytics.kotlin.android.utilities.toJSONObject
+import com.segment.analytics.kotlin.core.*
+import com.segment.analytics.kotlin.core.platform.DestinationPlugin
 import com.segment.analytics.kotlin.core.platform.Plugin
+import com.segment.analytics.kotlin.core.platform.plugins.LogType
 import com.segment.analytics.kotlin.core.platform.plugins.log
 import com.segment.analytics.kotlin.core.utilities.*
+import kotlinx.serialization.*
 import kotlinx.serialization.json.*
 import java.util.*
+
+/*
+This is an example of the Mixpanel device-mode destination plugin that can be integrated with
+Segment analytics.
+Note: This plugin is NOT SUPPORTED by Segment.  It is here merely as an example,
+and for your convenience should you find it useful.
+To use it in your codebase, we suggest copying this file over and include the following
+dependencies in your `build.gradle` file:
+```
+dependencies {
+    ...
+    api 'com.mixpanel.android:mixpanel-android:5.8.7'
+}
+```
+MIT License
+Copyright (c) 2021 Segment
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+@Serializable
+data class MixpanelSettings(
+    var token: String,
+    @SerialName("people")
+    var isPeopleEnabled: Boolean = false,
+    var setAllTraitsByDefault: Boolean = true,
+    var consolidatedPageCalls: Boolean = true,
+    var trackAllPages: Boolean = false,
+    var trackCategorizedPages: Boolean = false,
+    var trackNamedPages: Boolean = false,
+    @SerialName("peopleProperties")
+    var peoplePropertiesFilter: Set<String> = setOf(),
+    @SerialName("superProperties")
+    var superPropertiesFilter: Set<String> = setOf(),
+    @SerialName("eventIncrements")
+    var increments: Set<String> = setOf()
+)
 
 class MixpanelDestination(
     private val context: Context
 ) : DestinationPlugin(), AndroidLifecycle {
+
+    internal var settings: MixpanelSettings? = null
+    internal var mixpanel: MixpanelAPI? = null
+
     override val key: String = "Mixpanel"
-    private var token: String = ""
-    private lateinit var mixpanel: MixpanelAPI
 
-    companion object {
-        private val TRAITS_MAPPER: Map<String, String> = mapOf(
-            "email" to "\$email",
-            "phone" to "\$phone",
-            "firstName" to "\$first_name",
-            "lastName" to "\$last_name",
-            "name" to "\$name",
-            "username" to "\$username",
-            "createdAt" to "\$created",
-        )
+    override fun update(settings: Settings, type: Plugin.UpdateType) {
+        super.update(settings, type)
+        if (settings.isDestinationEnabled(key)) {
+            analytics.log("Mixpanel Destination is enabled")
+            this.settings = settings.destinationSettings(key)
+            if (type == Plugin.UpdateType.Initial) {
+                mixpanel = MixpanelAPI.getInstance(context, this.settings?.token)
+                analytics.log("Mixpanel Destination loaded")
+            }
+        } else {
+            analytics.log("Mixpanel destination is disabled via settings")
+        }
     }
 
-    // Configuration
-    private var isPeopleEnabled = false
-    private var setAllTraitsByDefault = false
-    private var consolidatedPageCalls = false
-    private var trackAllPages = false
-    private var trackCategorizedPages = false
-    private var trackNamedPages = false
-    private var peoplePropertiesFilter: Set<String> = setOf("peopleProperties")
-    private var superPropertiesFilter: Set<String> = setOf("superProperties")
-    private var increments: Set<String> = setOf("superProperties")
-
-    override fun track(payload: TrackEvent): BaseEvent {
+    override fun track(payload: TrackEvent): BaseEvent? {
+        val settings = settings ?: return null
+        // Example of transforming event property keys
         val eventName = payload.event
+        val properties = payload.properties
 
-        trackEvent(eventName, payload.properties)
+        trackEvent(eventName, properties)
 
-        if (isPeopleEnabled && increments.contains(eventName)) {
-            mixpanel.people.increment(eventName, 1.0)
-            mixpanel.people.set("Last $eventName", Date())
-        }
-
-        return payload
-    }
-
-    override fun identify(payload: IdentifyEvent): BaseEvent {
-        val userId = payload.userId
-        mixpanel.identify(userId)
-        analytics.log("mixpanel.identify($userId)", type = LogType.INFO, event = payload)
-
-        // Filter based on configuration and transform keys using PROPERTY_MAPPER
-        val peopleProperties = if (setAllTraitsByDefault || peoplePropertiesFilter.isEmpty()) {
-            payload.traits
-        } else {
-            payload.traits.filter { (k, _) -> peoplePropertiesFilter.contains(k) }
-        }.mapKeys { TRAITS_MAPPER[it.key] ?: it.key }
-
-        // Filter based on configuration and transform keys using PROPERTY_MAPPER
-        val superProperties = if (setAllTraitsByDefault || superPropertiesFilter.isEmpty()) {
-            payload.traits
-        } else {
-            payload.traits.filter { (k, _) -> superPropertiesFilter.contains(k) }
-        }.mapKeys { TRAITS_MAPPER[it.key] ?: it.key }
-
-        mixpanel.registerSuperProperties(superProperties.toJSONObject())
-        analytics.log(
-            "mixpanel.registerSuperProperties($superProperties)",
-            type = LogType.INFO,
-            event = payload
-        )
-
-        if (isPeopleEnabled) {
-            mixpanel.people.identify(userId)
-            analytics.log(
-                "mixpanel.people.identify($userId)",
-                type = LogType.INFO,
-                event = payload
-            )
-            mixpanel.people.set(peopleProperties.toJSONObject())
-            analytics.log(
-                "mixpanel.getPeople().set($peopleProperties)",
-                type = LogType.INFO,
-                event = payload
-            )
-        }
-
-        return payload
-    }
-
-    override fun screen(payload: ScreenEvent): BaseEvent {
-        if (consolidatedPageCalls) {
-            val props = buildJsonObject {
-                putAll(payload.properties)
-                put("name", payload.name)
+        with(settings) {
+            if (isPeopleEnabled && increments.contains(eventName)) {
+                mixpanel?.people?.increment(eventName, 1.0)
+                mixpanel?.people?.set("Last $eventName", Date())
             }
-            trackEvent("Loaded a Screen", props)
-        } else if (trackAllPages) {
-            trackEvent("Viewed ${payload.name} Screen", payload.properties)
-        } else if (trackCategorizedPages && payload.category.isNotEmpty()) {
-            trackEvent("Viewed ${payload.category} Screen", payload.properties)
-        } else if (trackNamedPages && payload.name.isNotEmpty()) {
-            trackEvent("Viewed ${payload.name} Screen", payload.properties)
         }
+
         return payload
     }
 
-    override fun group(payload: GroupEvent): BaseEvent {
-        val payloadGroupName = payload.traits["name"]?.jsonPrimitive?.let {
-            if (it.isString) {
-                it.content
+    override fun identify(payload: IdentifyEvent): BaseEvent? {
+        val settings = settings ?: return null
+        val userId: String = payload.userId
+        val traits: JsonObject = payload.traits
+
+        if (userId.isNotEmpty()) {
+            mixpanel?.identify(userId)
+            analytics.log("mixpanel.identify($userId)", type = LogType.INFO, event = payload)
+        }
+
+        with(settings) {
+            // Filter based on configuration
+            val peopleProperties = if (setAllTraitsByDefault) {
+                traits
             } else {
-                null
+                traits.filter { (k, _) -> peoplePropertiesFilter.contains(k) }
+            }.map(TRAIT_MAPPER)
+
+            // Filter based on configuration
+            val superProperties = if (setAllTraitsByDefault) {
+                traits
+            } else {
+                traits.filter { (k, _) -> superPropertiesFilter.contains(k) }
+            }.map(TRAIT_MAPPER)
+
+            if (superProperties.isNotEmpty()) {
+                mixpanel?.registerSuperProperties(superProperties.toJSONObject())
+                analytics.log(
+                    "mixpanel.registerSuperProperties($superProperties)",
+                    type = LogType.INFO,
+                    event = payload
+                )
+            }
+
+            if (isPeopleEnabled) {
+                mixpanel?.people?.identify(userId)
+                analytics.log(
+                    "mixpanel.people.identify($userId)",
+                    type = LogType.INFO,
+                    event = payload
+                )
+                if (peopleProperties.isNotEmpty()) {
+                    mixpanel?.people?.set(peopleProperties.toJSONObject())
+                    analytics.log(
+                        "mixpanel.getPeople().set($peopleProperties)",
+                        type = LogType.INFO,
+                        event = payload
+                    )
+                }
             }
         }
+
+        return payload
+    }
+
+    override fun group(payload: GroupEvent): BaseEvent? {
+        val groupId = payload.groupId
+        val traits = payload.traits
+
+        val payloadGroupName = traits.getString("name")
         val groupName = if (payloadGroupName.isNullOrEmpty()) {
             "[Segment] Group"
         } else {
             payloadGroupName
         }
-        val groupId = payload.groupId
 
         // Set Group Traits
-        if (payload.traits.isNotEmpty()) {
-            mixpanel.getGroup(groupName, groupId).setOnce(payload.traits.toJSONObject())
+        if (traits.isNotEmpty()) {
+            mixpanel?.getGroup(groupName, groupId)?.setOnce(traits.toJSONObject())
         }
 
-        mixpanel.setGroup(groupName, groupId)
+        mixpanel?.setGroup(groupName, groupId)
         analytics.log(
             "mixpanel.setGroup($groupName, $groupId)",
             type = LogType.INFO,
@@ -149,61 +188,84 @@ class MixpanelDestination(
         return payload
     }
 
-    override fun alias(payload: AliasEvent): BaseEvent {
+    override fun alias(payload: AliasEvent): BaseEvent? {
+        val userId = payload.userId
         val previousId = if (payload.previousId == payload.anonymousId) {
             // Instead of using our own anonymousId, we use Mixpanel's own generated Id.
-            mixpanel.distinctId
+            mixpanel?.distinctId
         } else {
             payload.previousId
         }
-        val userId = payload.userId
         if (userId.isNotBlank()) {
-            mixpanel.alias(userId, previousId)
+            mixpanel?.alias(userId, previousId)
             analytics.log("mixpanel.alias($userId, $previousId)", type = LogType.INFO, event = payload)
         }
         return payload
     }
 
-    override fun update(settings: Settings, type:Plugin.UpdateType) {
-        // if we've already set up this singleton SDK, can't do it again, so skip.
-        if (type != Plugin.UpdateType.Initial) return
+    override fun screen(payload: ScreenEvent): BaseEvent? {
+        val settings = settings ?: return null
+        val screenName = payload.name
+        val properties = payload.properties
+        val screenCategory = payload.category
 
-        super.update(settings, type)
-        val mixpanelSettings = settings.integrations[key]
-        mixpanelSettings?.jsonObject?.let {
-            analytics.log("mixpanel.received settings=$it", type = LogType.INFO)
-            consolidatedPageCalls = it.getBoolean("consolidatedPageCalls") ?: true
-            trackAllPages = it.getBoolean("trackAllPages") ?: true
-            trackCategorizedPages = it.getBoolean("trackCategorizedPages") ?: true
-            trackNamedPages = it.getBoolean("trackNamedPages") ?: true
-            isPeopleEnabled = it.getBoolean("people") ?: true
-            consolidatedPageCalls = it.getBoolean("consolidatedPageCalls") ?: true
-            setAllTraitsByDefault = it.getBoolean("setAllTraitsByDefault") ?: true
-
-            token = it.getString("token") ?: ""
-            increments = it.getStringSet("increments") ?: emptySet()
-            peoplePropertiesFilter = it.getStringSet("peopleProperties") ?: emptySet()
-            superPropertiesFilter = it.getStringSet("superProperties") ?: emptySet()
+        with(settings) {
+            if (consolidatedPageCalls) {
+                val props = buildJsonObject {
+                    putAll(properties)
+                    put("name", screenName)
+                }
+                trackEvent("Loaded a Screen", props)
+            } else if (trackAllPages) {
+                trackEvent("Viewed $screenName Screen", properties)
+            } else if (trackCategorizedPages && screenCategory.isNotEmpty()) {
+                trackEvent("Viewed $screenCategory Screen", properties)
+            } else if (trackNamedPages && screenName.isNotEmpty()) {
+                trackEvent("Viewed $screenName Screen", properties)
+            }
         }
-        mixpanel = MixpanelAPI.getInstance(context, token)
+
+        return payload
     }
 
     override fun onActivityCreated(activity: Activity?, savedInstanceState: Bundle?) {
+        val settings = settings ?: return
         // This is needed to trigger a call to #checkIntentForInboundAppLink.
         // From Mixpanel's source, this won't trigger a creation of another instance. It caches
         // instances by the application context and token, both of which remain the same.
-        MixpanelAPI.getInstance(activity, token);
+        MixpanelAPI.getInstance(activity, settings.token);
+    }
+
+    companion object {
+        // Rules for transforming a track event name
+        private val TRAIT_MAPPER = mapOf(
+            "email" to "\$email",
+            "phone" to "\$phone",
+            "firstName" to "\$first_name",
+            "lastName" to "\$last_name",
+            "name" to "\$name",
+            "username" to "\$username",
+            "createdAt" to "\$created"
+        )
     }
 
     private fun trackEvent(name: String, properties: JsonObject) {
         val props = properties.toJSONObject()
-        mixpanel.track(name, props)
+        mixpanel?.track(name, props)
         analytics.log("mixpanel.track($name, $properties)", type = LogType.INFO)
 
-        val revenue = properties["revenue"]?.jsonPrimitive?.double
-        if (isPeopleEnabled && revenue != null && revenue != 0.0) {
-            mixpanel.people.trackCharge(revenue, props)
-            analytics.log("mixpanel.people.trackCharge($name, $props)", type = LogType.INFO)
+        val revenue = properties.getDouble("revenue")
+
+        with(settings!!) {
+            if (isPeopleEnabled && revenue != null && revenue != 0.0) {
+                mixpanel?.people?.trackCharge(revenue, props)
+                analytics.log("mixpanel.people.trackCharge($name, $props)", type = LogType.INFO)
+            }
         }
     }
+
+    private fun Map<String, JsonElement>.map(
+        keyMapper: Map<String, String>,
+    ): Map<String, JsonElement> = JsonObject(this).mapTransform(keyMapper, null)
+
 }
