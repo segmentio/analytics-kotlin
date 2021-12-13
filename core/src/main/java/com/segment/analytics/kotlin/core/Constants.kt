69a31c91c1277ad3679be3586f1ae8c6c940f9e4
Wenxi Zeng <wenxi.zeng@segment.com>
2021-12-13 15:00:04 -0800

    update version to 1.4.1

--- core/src/main/java/com/segment/analytics/kotlin/core/Constants.kt
+++ core/src/main/java/com/segment/analytics/kotlin/core/Constants.kt
@@ -1,7 +1,7 @@
 package com.segment.analytics.kotlin.core
 
 object Constants {
-    const val LIBRARY_VERSION = "1.4.0"
+    const val LIBRARY_VERSION = "1.4.1"
     const val DEFAULT_API_HOST = "api.segment.io/v1"
     const val DEFAULT_CDN_HOST = "cdn-settings.segment.com/v1"
 }
