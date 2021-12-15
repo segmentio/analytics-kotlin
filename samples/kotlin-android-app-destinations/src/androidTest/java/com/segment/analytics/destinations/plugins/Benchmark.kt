4281993422c0412b5775797ebbf62c0d83231ba1
Prayansh Srivastava <prayansh@gmail.com>
2021-12-15 14:25:28 -0800

    good spot

--- samples/kotlin-android-app-destinations/src/androidTest/java/com/segment/analytics/destinations/plugins/Benchmark.kt
+++ samples/kotlin-android-app-destinations/src/androidTest/java/com/segment/analytics/destinations/plugins/Benchmark.kt
@@ -1,29 +1,67 @@
 package com.segment.analytics.destinations.plugins
 
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import com.segment.analytics.kotlin.core.BaseEvent
 import com.segment.analytics.kotlin.core.TrackEvent
 import kotlinx.serialization.json.buildJsonObject
 import kotlinx.serialization.json.put
 import kotlin.system.measureTimeMillis
 import com.eclipsesource.v8.V8
+import com.eclipsesource.v8.V8Array
+import com.eclipsesource.v8.V8Function
+import com.eclipsesource.v8.V8Object
+import com.eclipsesource.v8.utils.V8ObjectUtils
+import com.segment.analytics.kotlin.core.emptyJsonObject
+import com.segment.analytics.kotlin.core.utilities.toContent
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.encodeToJsonElement
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertNull
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.io.BufferedReader
+import java.util.Date
 
 
 @RunWith(AndroidJUnit4::class)
 class Benchmark {
     val e1 = TrackEvent(
-        event = "App Opened",
-        properties = buildJsonObject { put("new", true); put("click", true) })
+        event = "CP VOD - Start video",
+        properties = buildJsonObject { put("new", true); put("click", true) }
+    ).apply {
+        messageId = "qwerty-1234"
+        anonymousId = "anonId"
+        integrations = emptyJsonObject
+        context = emptyJsonObject
+        timestamp = Date(0).toInstant().toString()
+    }
     val e2 = TrackEvent(
         event = "Screen Opened",
-        properties = buildJsonObject { put("new", false); put("click", false) })
+        properties = buildJsonObject { put("new", false); put("click", false) }
+    ).apply {
+        messageId = "qwerty-1234"
+        anonymousId = "anonId"
+        integrations = emptyJsonObject
+        context = emptyJsonObject
+        timestamp = Date(0).toInstant().toString()
+    }
     val e3 = TrackEvent(
         event = "App Closed",
-        properties = buildJsonObject { put("new", false); put("click", true) })
+        properties = buildJsonObject { put("new", false); put("click", true) }
+    ).apply {
+        messageId = "qwerty-1234"
+        anonymousId = "anonId"
+        integrations = emptyJsonObject
+        context = emptyJsonObject
+        timestamp = Date(0).toInstant().toString()
+    }
+
+    private val appContext = InstrumentationRegistry.getInstrumentation().context
 
     @Test
     fun usingExpressions() {
@@ -31,7 +69,20 @@ class Benchmark {
             matchers.add(MatcherExpressionImpl())
         }
         val matcherTime = measureTimeMillis {
-            assertNotNull(plugin.execute(e1))
+            assertNull(plugin.execute(e1))
+            assertNull(plugin.execute(e2))
+            assertNotNull(plugin.execute(e3))
+        }
+        println(matcherTime)
+    }
+
+    @Test
+    fun usingFQlQuery() {
+//        val plugin = FQLDropPlugin("[\"!\",[\"or\",[\"=\",\"event\",{\"value\":\"App Opened\"}],[\"=\",\"event\",{\"value\":\"App Closed\"}]]]")
+        val plugin =
+            FQLDropPlugin("""["!",["or",["=","event",{"value":"App Opened"}],["=","event",{"value":"App Closed"}]]]""")
+        val matcherTime = measureTimeMillis {
+            assertNull(plugin.execute(e1))
             assertNull(plugin.execute(e2))
             assertNotNull(plugin.execute(e3))
         }
@@ -40,15 +91,14 @@ class Benchmark {
 
     @Test
     fun usingJsRuntime() {
+        val localJSMiddlewareInputStream = appContext.assets.open("sample.js")
+        val script = localJSMiddlewareInputStream.bufferedReader().use(BufferedReader::readText)
         val plugin = DestinationFiltersDropPlugin().apply {
-            matchers.add(MatcherJSImpl())
+            matchers.add(MatcherJSImpl(script))
         }
-        val matcherTime = measureTimeMillis {
-            assertNotNull(plugin.execute(e1))
-            assertNull(plugin.execute(e2))
-            assertNotNull(plugin.execute(e3))
-        }
-        println(matcherTime)
+        plugin.execute(e1)
+        plugin.execute(e2)
+        plugin.execute(e3)
     }
 }
 
@@ -66,24 +116,60 @@ class MatcherExpressionImpl : Matcher {
         payload as TrackEvent
         return Not(
             Or(
-                Equals(Path("event"), "App Opened"),
-                Equals(Path("event"), "App Closed")
+                Equals(
+                    FQLValue.Path(payload, "event"),
+                    FQLValue.Value(JsonPrimitive("App Opened"))
+                ),
+                Equals(
+                    FQLValue.Path(payload, "event"),
+                    FQLValue.Value(JsonPrimitive("App Closed"))
+                )
             )
         ).evaluate(payload)
     }
 }
 
-class MatcherJSImpl : Matcher {
+class MatcherJSImpl(private val script: String) : Matcher {
 
-    val runtime = V8.createV8Runtime()
+    internal class Console {
+        fun log(message: String) {
+            println("[INFO] $message")
+        }
+
+        fun error(message: String) {
+            println("[ERROR] $message")
+        }
+    }
+
+    private val runtime: V8 = V8.createV8Runtime().also {
+        val console = Console()
+        val v8Console = V8Object(it)
+        // todo Allow string array
+        v8Console.registerJavaMethod(console, "log", "log", arrayOf<Class<*>>(String::class.java))
+        v8Console.registerJavaMethod(console, "error", "err", arrayOf<Class<*>>(String::class.java))
+        it.add("console", v8Console)
+    }
+
+    fun getObject(key: String): V8Object {
+        var result = runtime.getObject(key)
+        if (result.isUndefined) {
+            result = runtime.executeObjectScript(key) // Blows up when the key does not exist
+        }
+        return result
+    }
+
     override fun match(payload: BaseEvent): Boolean {
         payload as TrackEvent
-        val result = runtime.executeBooleanScript(
-            """
-              !("${payload.event}" === "App Opened" || "${payload.event}" === "App Closed")
-              """.trimIndent()
-        )
+        runtime.executeScript(script)
+
+        val fn = getObject("edge_function.fnMatch") as V8Function
+        val params = V8Array(runtime.runtime)
+        val payloadJson = (Json.encodeToJsonElement(payload) as JsonObject).toContent()
+        params.push(V8ObjectUtils.toV8Object(runtime.runtime, payloadJson))
+        // call it and pick up the result
+        val fnResult = fn.call(null, params) as String
+        println(fnResult)
 //        runtime.close()
-        return result
+        return true
     }
 }
--- samples/kotlin-android-app-destinations/src/main/java/com/segment/analytics/destinations/plugins/DestinationFiltersPlugin.kt
+++ samples/kotlin-android-app-destinations/src/main/java/com/segment/analytics/destinations/plugins/DestinationFiltersPlugin.kt
@@ -2,7 +2,16 @@ package com.segment.analytics.destinations.plugins
 
 import com.segment.analytics.kotlin.core.Analytics
 import com.segment.analytics.kotlin.core.BaseEvent
+import com.segment.analytics.kotlin.core.TrackEvent
 import com.segment.analytics.kotlin.core.platform.Plugin
+import com.segment.analytics.kotlin.core.utilities.toContent
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonNull
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.encodeToJsonElement
 
 /*
 {
@@ -45,6 +54,27 @@ class DestinationFiltersDropPlugin : Plugin {
     }
 }
 
+sealed class FQLValue {
+    abstract fun value(): JsonElement
+
+    class Value(private val value: JsonElement) : FQLValue() {
+        override fun value(): JsonElement {
+            return value
+        }
+    }
+
+    class Path(private val event: BaseEvent, private val path: String) : FQLValue() {
+        override fun value(): JsonElement {
+            return if (path == "event") {
+                event as TrackEvent
+                JsonPrimitive(event.event)
+            } else {
+                JsonPrimitive("")
+            }
+        }
+    }
+}
+
 interface Matcher {
     /*
     {
@@ -80,16 +110,104 @@ class And(private val e1: Expression, private val e2: Expression) : Expression {
     }
 }
 
-class Equals(private val t1: Any, private val t2: Any): Expression {
+class Equals(private val t1: FQLValue, private val t2: FQLValue) : Expression {
     override fun evaluate(event: BaseEvent): Boolean {
-        return t1 == t2
-    }
-}
-
-class Path(private val path: String): Expression {
-    override fun evaluate(event: BaseEvent): Boolean {
-        return
+        return t1.value() == t2.value()
     }
 }
 
 // Not(Or(Equals(payload.event, "CP VOD - Start video"), Equals(payload.event, "CP VOD - Track video")))
+
+class FQLDropPlugin(fqlQuery: String) : Plugin {
+    override val type: Plugin.Type = Plugin.Type.Enrichment
+    override lateinit var analytics: Analytics
+
+    val fqlIR: JsonArray = Json.decodeFromString(JsonArray.serializer(), fqlQuery)
+
+    // Implement only drop event
+    override fun execute(event: BaseEvent): BaseEvent? {
+        if (fqlEvaluate(fqlIR, event)) {
+            return null
+        }
+        return super.execute(event)
+    }
+}
+
+//fun JsonArray.tail(n: Int): JsonArray {
+//    return JsonArray(drop(n))
+//}
+
+// "["!",["or",["=","event",{"value":"CP VOD - Start video"}],["=","event",{"value":"CP VOD - Track video"}]]]",
+// Array("!", Array("or", Array("=", "event", {"value":"CP VOD"}), Array("=", "event", {"value":"CP VOD"})))
+fun fqlEvaluate(ir: JsonElement, payload: BaseEvent): Boolean {
+    // check if the given value is literally `true`
+    if (ir !is JsonArray) {
+        return getValue(ir, payload) == true
+    }
+    val item = ir[0].toContent()
+    when (item) {
+        /** Unary Cases  **/
+        "!" -> {
+            return !fqlEvaluate(ir[1], payload)
+        }
+        /** Binary Cases **/
+        "or" -> {
+            for (i in 1 until ir.size) {
+                if (fqlEvaluate(ir[i], payload)) {
+                    return true
+                }
+            }
+            return false
+        }
+        "and" -> {
+            for (i in 1 until ir.size) {
+                if (fqlEvaluate(ir[i], payload)) {
+                    return false
+                }
+            }
+            return true
+        }
+        /** Equivalence **/
+        "=" -> {
+            return compareItems(ir[1], ir[2], payload)
+        }
+        "!=" -> {
+            return !compareItems(ir[1], ir[2], payload)
+        }
+        else -> {
+            throw Error("FQL IR could not evaluate for token: $item")
+        }
+    }
+}
+
+fun getValue(ir: JsonElement, payload: BaseEvent): Any {
+    return when (ir) {
+        is JsonArray -> {
+            // leave as is
+            return ir
+        }
+        is JsonObject -> {
+            // it is of the form {"value": VALUE}
+            return ir["value"]?.toContent() ?: throw Error("FQL IR could not parse $ir")
+        }
+        is JsonPrimitive -> {
+            // probably an event property
+            // TODO write more complex logic here
+            val content = ir.toContent()
+            if (content == "event") {
+                payload as TrackEvent
+                return payload.event
+            } else {
+                ""
+            }
+        }
+    }
+}
+
+fun compareItems(first: JsonElement, second: JsonElement, payload: BaseEvent): Boolean {
+    val firstVal = getValue(first, payload)
+    val secondVal = getValue(second, payload)
+    // TODO check if IR
+
+    return firstVal == secondVal
+}
