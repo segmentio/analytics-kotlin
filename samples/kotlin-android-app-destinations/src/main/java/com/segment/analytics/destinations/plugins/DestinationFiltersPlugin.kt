6f188edb16c45020780dc5468b64dae5f1e0d450
Prayansh Srivastava <prayansh@gmail.com>
2022-01-05 10:13:11 -0800

    push all the code

--- samples/kotlin-android-app-destinations/src/main/java/com/segment/analytics/destinations/plugins/DestinationFiltersPlugin.kt
+++ samples/kotlin-android-app-destinations/src/main/java/com/segment/analytics/destinations/plugins/DestinationFiltersPlugin.kt
@@ -1,144 +1,147 @@
 package com.segment.analytics.destinations.plugins
 
-import com.segment.analytics.kotlin.core.Analytics
 import com.segment.analytics.kotlin.core.BaseEvent
 import com.segment.analytics.kotlin.core.TrackEvent
-import com.segment.analytics.kotlin.core.platform.Plugin
 import com.segment.analytics.kotlin.core.utilities.toContent
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonElement
-import kotlinx.serialization.json.JsonNull
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.JsonPrimitive
-import kotlinx.serialization.json.encodeToJsonElement
+import kotlinx.serialization.json.decodeFromJsonElement
 
-/*
-{
-  "matchers": [
-    {
-      "ir": "[\"!\",[\"or\",[\"=\",\"event\",{\"value\":\"CP VOD - Start video\"}],[\"=\",\"event\",{\"value\":\"CP VOD - Track video\"}]]]",
-      "type": "fql",
-      "config": {
-        "expr": "!(event = \"CP VOD - Start video\" or event = \"CP VOD - Track video\")"
-      }
-    }
-  ],
-  "scope": "destinations",
-  "target_type": "workspace::project::destination::config",
-  "transformers": [
-    [
-      {
-        "type": "drop"
-      }
-    ]
-  ],
-  "destinationName": "Amazon Kinesis"
-}
- */
-
-class DestinationFiltersDropPlugin : Plugin {
-    override val type: Plugin.Type = Plugin.Type.Enrichment
-    override lateinit var analytics: Analytics
-
-    val matchers = mutableListOf<Matcher>()
-
-    // Implement only drop event
-    override fun execute(event: BaseEvent): BaseEvent? {
-        for (matcher in matchers) {
-            if (matcher.match(event)) {
-                return null
-            }
-        }
-        return super.execute(event)
-    }
-}
-
-sealed class FQLValue {
-    abstract fun value(): JsonElement
-
-    class Value(private val value: JsonElement) : FQLValue() {
-        override fun value(): JsonElement {
-            return value
-        }
-    }
-
-    class Path(private val event: BaseEvent, private val path: String) : FQLValue() {
-        override fun value(): JsonElement {
-            return if (path == "event") {
-                event as TrackEvent
-                JsonPrimitive(event.event)
-            } else {
-                JsonPrimitive("")
-            }
-        }
-    }
-}
-
-interface Matcher {
-    /*
-    {
-      "ir": "["!",["or",["=","event",{"value":"CP VOD - Start video"}],["=","event",{"value":"CP VOD - Track video"}]]]",
-      "type": "fql",
-      "config": {
-        "expr": "!(event = \"CP VOD - Start video\" or event = \"CP VOD - Track video\")"
-      }
-    }
-     */
-    fun match(payload: BaseEvent): Boolean
-}
-
-interface Expression {
-    fun evaluate(event: BaseEvent): Boolean
-}
-
-class Not(private val e: Expression) : Expression {
-    override fun evaluate(event: BaseEvent): Boolean {
-        return !e.evaluate(event)
-    }
-}
-
-class Or(private val e1: Expression, private val e2: Expression) : Expression {
-    override fun evaluate(event: BaseEvent): Boolean {
-        return e1.evaluate(event) || e2.evaluate(event)
-    }
-}
-
-class And(private val e1: Expression, private val e2: Expression) : Expression {
-    override fun evaluate(event: BaseEvent): Boolean {
-        return e1.evaluate(event) && e2.evaluate(event)
-    }
-}
-
-class Equals(private val t1: FQLValue, private val t2: FQLValue) : Expression {
-    override fun evaluate(event: BaseEvent): Boolean {
-        return t1.value() == t2.value()
-    }
-}
-
-// Not(Or(Equals(payload.event, "CP VOD - Start video"), Equals(payload.event, "CP VOD - Track video")))
-
-class FQLDropPlugin(fqlQuery: String) : Plugin {
-    override val type: Plugin.Type = Plugin.Type.Enrichment
-    override lateinit var analytics: Analytics
-
-    val fqlIR: JsonArray = Json.decodeFromString(JsonArray.serializer(), fqlQuery)
-
-    // Implement only drop event
-    override fun execute(event: BaseEvent): BaseEvent? {
-        if (fqlEvaluate(fqlIR, event)) {
-            return null
-        }
-        return super.execute(event)
-    }
-}
-
-//fun JsonArray.tail(n: Int): JsonArray {
-//    return JsonArray(drop(n))
+//
+//class FQLRuntimePlugin(private val destinationKey: String) : Plugin {
+//    override val type: Plugin.Type = Plugin.Type.Enrichment
+//    override lateinit var analytics: Analytics
+//
+//    var fqlStore: FQLStore? = null
+//    var rulesToApply: List<RoutingRule> = emptyList()
+//
+//    override fun update(settings: Settings, type: Plugin.UpdateType) {
+//        super.update(settings, type)
+//        updateFQLStore(JsonArray(emptyList()))
+//    }
+//
+//    fun updateFQLStore(routingRules: JsonArray) {
+//        fqlStore = FQLStore(routingRules)
+//        fqlStore?.getRulesForDestination(destinationKey)?.let {
+//            rulesToApply = it
+//        }
+//    }
+//
+//    // Implement only drop event
+//    override fun execute(event: BaseEvent): BaseEvent? {
+//        for (routingRule in rulesToApply) {
+//            for (matcher in routingRule.matchers) {
+//                val ir = Json.decodeFromString(JsonArray.serializer(), matcher.ir)
+//                if (fqlEvaluate(ir, event)) {
+//                    return null
+//                }
+//            }
+//        }
+//        return super.execute(event)
+//    }
+//}
+//
+//class FQLCompiledPlugin(private val destinationKey: String) : Plugin {
+//    override val type: Plugin.Type = Plugin.Type.Enrichment
+//    override lateinit var analytics: Analytics
+//
+//    var fqlStore: FQLStore? = null
+//
+//    override fun update(settings: Settings, type: Plugin.UpdateType) {
+//        super.update(settings, type)
+//        updateFQLStore(JsonArray(emptyList()))
+//    }
+//
+//    fun updateFQLStore(routingRules: JsonArray) {
+//        fqlStore = FQLStore(routingRules)
+//    }
+//
+//    // Implement only drop event
+//    override fun execute(event: BaseEvent): BaseEvent? {
+//        fqlStore?.getRulesForDestination(destinationKey)?.let {
+//            for (routingRule in it) {
+//                for (matcher in routingRule.matchers) {
+//                    val ir = Json.decodeFromString(JsonArray.serializer(), matcher.ir)
+//                    if (fqlEvaluate(ir, event)) {
+//                        return null
+//                    }
+//                }
+//            }
+//        }
+//        return super.execute(event)
+//    }
 //}
 
-// "["!",["or",["=","event",{"value":"CP VOD - Start video"}],["=","event",{"value":"CP VOD - Track video"}]]]",
-// Array("!", Array("or", Array("=", "event", {"value":"CP VOD"}), Array("=", "event", {"value":"CP VOD"})))
+typealias Predicate<T> = (payload: T) -> Boolean
+
+fun compile(ir: JsonElement): Predicate<BaseEvent> {
+    // check if the given value is literally `true`
+    if (ir !is JsonArray) {
+        return { event: BaseEvent ->
+            getValue(ir, event) == true
+        }
+    }
+    val item = ir[0].toContent()
+    when (item) {
+        /** Unary Cases  **/
+        "!" -> {
+            return { event: BaseEvent ->
+                val filter = compile(ir[1])
+                !filter(event)
+            }
+        }
+        /** Binary Cases **/
+        "or" -> {
+            val predicates = (1 until ir.size).map {
+                compile(ir[it])
+            }
+            return { event: BaseEvent ->
+                var result = false
+                for (predicate in predicates) {
+                    if (predicate(event)) {
+                        result = true
+                        break
+                    }
+                }
+                result
+            }
+        }
+        "and" -> {
+            val predicates = (1 until ir.size).map {
+                compile(ir[it])
+            }
+            return { event: BaseEvent ->
+                var result = true
+                for (predicate in predicates) {
+                    if (!predicate(event)) {
+                        result = false
+                        break
+                    }
+                }
+                result
+            }
+        }
+        /** Equivalence **/
+        "=" -> {
+            return { event: BaseEvent ->
+                getValue(ir[1], event) == getValue(ir[2], event)
+            }
+        }
+        "!=" -> {
+            return { event: BaseEvent ->
+                getValue(ir[1], event) != getValue(ir[2], event)
+            }
+        }
+        else -> {
+            throw Error("FQL IR could not evaluate for token: $item")
+        }
+    }
+}
+
 fun fqlEvaluate(ir: JsonElement, payload: BaseEvent): Boolean {
     // check if the given value is literally `true`
     if (ir !is JsonArray) {
@@ -210,4 +213,24 @@ fun compareItems(first: JsonElement, second: JsonElement, payload: BaseEvent): B
     // TODO check if IR
 
     return firstVal == secondVal
+}
+
+@Serializable
+data class Matcher(
+    val ir: String,
+    val type: String,
+)
+
+@Serializable
+data class RoutingRule(
+    val matchers: List<Matcher>,
+    val destinationName: String,
+)
+
+class FQLStore(routingRules: JsonArray) {
+    val rules: List<RoutingRule> = Json.decodeFromJsonElement(routingRules)
+
+    fun getRulesForDestination(key: String): List<RoutingRule> {
+        return rules.filter { it.destinationName == key }
+    }
 }
