57e48e31c2af8e808f9e4dcf002f7985af847b75
Wenxi Zeng <wenxi.zeng@segment.com>
2022-03-07 13:37:53 -0800

    remove supported destinations from sample

--- samples/kotlin-android-app-destinations/src/main/java/com/segment/analytics/kotlin/destinations/MainApplication.kt
+++ samples/kotlin-android-app-destinations/src/main/java/com/segment/analytics/kotlin/destinations/MainApplication.kt
@@ -4,7 +4,6 @@ import android.app.Application
 import com.segment.analytics.kotlin.destinations.plugins.*
 import com.segment.analytics.kotlin.android.Analytics
 import com.segment.analytics.kotlin.core.Analytics
-import com.segment.analytics.kotlin.core.platform.plugins.logger.*
 import java.util.concurrent.Executors
 
 class MainApplication : Application() {
@@ -23,8 +22,6 @@ class MainApplication : Application() {
             this.flushInterval = 0
         }
 
-        analytics.add(MixpanelDestination(applicationContext))
-
         // A random webhook url to view your events
         analytics.add(
             WebhookPlugin(
@@ -33,37 +30,8 @@ class MainApplication : Application() {
             )
         )
 
-        // Try out amplitude session
-        analytics.add(AmplitudeSession())
-
-        // Try out Firebase Destination
-        analytics.add(FirebaseDestination(applicationContext))
-
         // Try out Intercom destination
         analytics.add(IntercomDestination(this))
-
-        val appsflyerDestination = AppsFlyerDestination(applicationContext, true)
-        analytics.add(appsflyerDestination)
-
-        appsflyerDestination.conversionListener =
-            object : AppsFlyerDestination.ExternalAppsFlyerConversionListener {
-                override fun onConversionDataSuccess(map: Map<String, Any>) {
-                    // Process Deferred Deep Linking here
-                    for (attrName in map.keys) {
-                        analytics.log("Appsflyer: attribute: " + attrName + " = " + map[attrName])
-                    }
-                }
-
-                override fun onConversionDataFail(s: String?) {}
-                override fun onAppOpenAttribution(map: Map<String, String>) {
-                    // Process Direct Deep Linking here
-                    for (attrName in map.keys) {
-                        analytics.log("Appsflyer: attribute: " + attrName + " = " + map[attrName])
-                    }
-                }
-
-                override fun onAttributionFailure(s: String?) {}
-            }
 
     }
 }
