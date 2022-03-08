af2603c898135c2805919780a7784cf35adb2b79
Wenxi Zeng <wenxi.zeng@segment.com>
2022-03-08 13:54:21 -0600

    prepare release 1.5.0 (#76)
    * prepare release 1.5.0
    
    * prepare snapshot 1.5.1
    
    * add supported destination examples

--- samples/kotlin-android-app-destinations/src/main/java/com/segment/analytics/kotlin/destinations/MainApplication.kt
+++ samples/kotlin-android-app-destinations/src/main/java/com/segment/analytics/kotlin/destinations/MainApplication.kt
@@ -4,6 +4,11 @@ import android.app.Application
 import com.segment.analytics.kotlin.destinations.plugins.*
 import com.segment.analytics.kotlin.android.Analytics
 import com.segment.analytics.kotlin.core.Analytics
+import com.segment.analytics.kotlin.core.platform.plugins.logger.*
+import com.segment.analytics.kotlin.destinations.amplitude.AmplitudeSession
+import com.segment.analytics.kotlin.destinations.appsflyer.AppsFlyerDestination
+import com.segment.analytics.kotlin.destinations.firebase.FirebaseDestination
+import com.segment.analytics.kotlin.destinations.mixpanel.MixpanelDestination
 import java.util.concurrent.Executors
 
 class MainApplication : Application() {
@@ -22,6 +27,8 @@ class MainApplication : Application() {
             this.flushInterval = 0
         }
 
+        analytics.add(MixpanelDestination(applicationContext))
+
         // A random webhook url to view your events
         analytics.add(
             WebhookPlugin(
@@ -30,8 +37,37 @@ class MainApplication : Application() {
             )
         )
 
+        // Try out amplitude session
+        analytics.add(AmplitudeSession())
+
+        // Try out Firebase Destination
+        analytics.add(FirebaseDestination(applicationContext))
+
         // Try out Intercom destination
         analytics.add(IntercomDestination(this))
+
+        val appsflyerDestination = AppsFlyerDestination(applicationContext, true)
+        analytics.add(appsflyerDestination)
+
+        appsflyerDestination.conversionListener =
+            object : AppsFlyerDestination.ExternalAppsFlyerConversionListener {
+                override fun onConversionDataSuccess(map: Map<String, Any>) {
+                    // Process Deferred Deep Linking here
+                    for (attrName in map.keys) {
+                        analytics.log("Appsflyer: attribute: " + attrName + " = " + map[attrName])
+                    }
+                }
+
+                override fun onConversionDataFail(s: String?) {}
+                override fun onAppOpenAttribution(map: Map<String, String>) {
+                    // Process Direct Deep Linking here
+                    for (attrName in map.keys) {
+                        analytics.log("Appsflyer: attribute: " + attrName + " = " + map[attrName])
+                    }
+                }
+
+                override fun onAttributionFailure(s: String?) {}
+            }
 
     }
 }
