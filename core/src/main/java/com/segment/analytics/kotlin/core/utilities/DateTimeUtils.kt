7dca34c73e40a6073507f9c49580a16c468b1b7f
Didier Garcia <didier.garcia@segment.com>
2023-04-19 00:24:02 -0400

    Add milliseconds to date time string and add documenation.

--- core/src/main/java/com/segment/analytics/kotlin/core/utilities/DateTimeUtils.kt
+++ core/src/main/java/com/segment/analytics/kotlin/core/utilities/DateTimeUtils.kt
@@ -3,8 +3,16 @@ package com.segment.analytics.kotlin.core.utilities
 import java.text.SimpleDateFormat
 import java.util.*
 
+/**
+ * This function is a replacement for Instant.now().toString(). It produces strings in a
+ * compatible format.
+ *
+ * Ex:
+ * Instant.now():       2023-04-19T04:03:46.880969Z
+ * dateTimeNowString(): 2023-04-19T04:03:46.880Z
+ */
 fun dateTimeNowString(): String {
-    val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:sszzz")
+    val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'.'Szzz")
     val utc = TimeZone.getTimeZone("UTC");
     sdf.timeZone = utc;
     return sdf.format(Date()).replace("UTC", "Z")
