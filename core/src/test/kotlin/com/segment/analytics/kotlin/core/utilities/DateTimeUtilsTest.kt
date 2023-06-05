fabeec98ead21ea9f04495546c054aac029ebc2c
Dennis Deng <dennis.deng@instacart.com>
2023-06-05 13:48:22 -0400

    Add test

--- core/src/test/kotlin/com/segment/analytics/kotlin/core/utilities/DateTimeUtilsTest.kt
+++ core/src/test/kotlin/com/segment/analytics/kotlin/core/utilities/DateTimeUtilsTest.kt
@@ -1,5 +1,14 @@
 package com.segment.analytics.kotlin.core.utilities
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
-internal class DateTimeUtilsTest
+class DateTimeUtilsTest {
+
+    @Test
+    fun `dateTimeNowString() produces a string in the correct format`() {
+        val dateTimeNowString = dateTimeNowString()
+        val regex = Regex("^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:\\d{2}\\.\\d{3}Z$")
+        assertTrue(regex.matches(dateTimeNowString))
+    }
+}
