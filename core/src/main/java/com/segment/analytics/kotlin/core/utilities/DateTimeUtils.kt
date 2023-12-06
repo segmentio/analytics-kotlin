416edf553c4371b7a2d8b7272d107437cd94828e
Wenxi Zeng <wenxi.zeng@segment.com>
2023-12-06 12:25:39 -0800

    make SegmentInstant thread safe (#207)

--- core/src/main/java/com/segment/analytics/kotlin/core/utilities/DateTimeUtils.kt
+++ core/src/main/java/com/segment/analytics/kotlin/core/utilities/DateTimeUtils.kt
@@ -3,30 +3,31 @@ package com.segment.analytics.kotlin.core.utilities
 import java.text.SimpleDateFormat
 import java.util.*
 
-object SegmentInstant {
-
-    // Note, we should specify locale = Locale.ROOT, otherwise the timestamp returned will use
-    // the default locale, which may not be what we want.
-    private val formatter: SimpleDateFormat =
-        SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'.'SSSzzz", Locale.ROOT).apply {
-            timeZone = TimeZone.getTimeZone("UTC")
+class SegmentInstant {
+    companion object {
+        private val formatters = object : ThreadLocal<SimpleDateFormat>() {
+            override fun initialValue(): SimpleDateFormat {
+                return SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'.'SSSzzz", Locale.ROOT).apply {
+                    timeZone = TimeZone.getTimeZone("UTC")
+                }
+            }
         }
 
-    /**
-     * This function is a replacement for Instant.now().toString(). It produces strings in a
-     * compatible format.
-     *
-     * Ex:
-     * Instant.now():       2023-04-19T04:03:46.880969Z
-     * dateTimeNowString(): 2023-04-19T04:03:46.880Z
-     */
-    fun now(): String {
-        return from(Date())
-    }
+        /**
+         * This function is a replacement for Instant.now().toString(). It produces strings in a
+         * compatible format.
+         *
+         * Ex:
+         * Instant.now():       2023-04-19T04:03:46.880969Z
+         * dateTimeNowString(): 2023-04-19T04:03:46.880Z
+         */
+        fun now(): String {
+            return from(Date())
+        }
 
-    internal fun from(date: Date): String {
-        // internal use only. for testing purpose.
-        return formatter.format(date).replace("UTC", "Z")
+        internal fun from(date: Date): String {
+            return formatters.get().format(date).replace("UTC", "Z")
+        }
     }
 }
 
