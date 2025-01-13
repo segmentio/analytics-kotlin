627dc803973f7588c4493f1373e06834cf9ccf5a
Wenxi Zeng <wzeng@twilio.com>
2025-01-13 16:22:29 -0600

    typo fix

--- android/src/main/java/com/segment/analytics/kotlin/android/Storage.kt
+++ android/src/main/java/com/segment/analytics/kotlin/android/Storage.kt
@@ -33,8 +33,8 @@ object AndroidStorageProvider : StorageProvider {
 
         if (params.size < 2 || params[0] !is Analytics || params[1] !is Context) {
             throw IllegalArgumentException("""
-                Invalid parameters for ConcreteStorageProvider. 
-                ConcreteStorageProvider requires at least 2 parameters.
+                Invalid parameters for AndroidStorageProvider. 
+                AndroidStorageProvider requires at least 2 parameters.
                  The first argument has to be an instance of Analytics,
                  an the second argument has to be an instance of Context
             """.trimIndent())
