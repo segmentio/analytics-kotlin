347333cfae28a9e910d50c7872afa5d79da2bcc3
Didier Garcia <digarcia@twilio.com>
2025-03-11 12:20:17 -0400

    Add deep link tests (#258)
    * refactor: uri parameter extraction into its own function.
    
    * test: new uri parameter extraction function in DeepLinkUtils.
    
    * refactor: use idomatic kotlin.

--- android/src/main/java/com/segment/analytics/kotlin/android/utilities/DeepLinkUtils.kt
+++ android/src/main/java/com/segment/analytics/kotlin/android/utilities/DeepLinkUtils.kt
@@ -1,7 +1,9 @@
 package com.segment.analytics.kotlin.android.utilities
 
 import android.content.Intent
+import android.net.Uri
 import com.segment.analytics.kotlin.core.Analytics
+import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.buildJsonObject
 import kotlinx.serialization.json.put
 
@@ -13,13 +15,32 @@ class DeepLinkUtils(val analytics: Analytics) {
             return
         }
 
-        val properties = buildJsonObject {
+        val properties = extractLinkProperties(referrer, intent.data)
+        analytics.track("Deep Link Opened", properties)
+    }
 
+    /**
+     * Builds a JsonObject with the parameters of a given Uri.
+     *
+     * Note: The Uri must be hierarchical (myUri.isHierarchical == true) for parameters to be
+     * extracted.
+     *
+     * Example hierarchical Uri: http://example.com/
+     * Example non-hierarchical Uri: mailto:me@email.com
+     *
+     * Note: we return the given Uri as a property named: "url" since this is what is expected
+     * upstream.
+     */
+    fun extractLinkProperties(
+        referrer: String?,
+        uri: Uri?
+    ): JsonObject {
+        val properties = buildJsonObject {
             referrer?.let {
                 put("referrer", it)
             }
 
-            intent.data?.let { uri ->
+            uri?.let {
                 if (uri.isHierarchical) {
                     for (parameter in uri.queryParameterNames) {
                         val value = uri.getQueryParameter(parameter)
@@ -31,6 +52,7 @@ class DeepLinkUtils(val analytics: Analytics) {
                 put("url", uri.toString())
             }
         }
-        analytics.track("Deep Link Opened", properties)
+
+        return properties
     }
 }
